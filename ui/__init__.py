0d3f4fc6e31d0c82bf3f217be9595f4bda8b8699
Ki11Cr0w <romero.jose79@gmail.com>
2026-01-15 12:26:43 -0300

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,11 +1,4 @@
 # ui/__init__.py
-from .componentes import (
-    mostrar_metricas_principales,
-    mostrar_tabla_resultados,
-    crear_uploader_multiple,
-    procesar_lote_archivos
-)
-
 from .vistas import (
     vista_carga_multiple_archivos,
     vista_resumen_compacto,
@@ -15,9 +8,5 @@ from .vistas import (
 __all__ = [
     'vista_carga_multiple_archivos',
     'vista_resumen_compacto',
-    'vista_resultados',
-    'mostrar_metricas_principales',
-    'mostrar_tabla_resultados',
-    'crear_uploader_multiple',
-    'procesar_lote_archivos'
+    'vista_resultados'
 ]
