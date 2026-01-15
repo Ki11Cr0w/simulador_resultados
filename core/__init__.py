4157825987e149f112ca97e393ff991321f78208
Ki11Cr0w <romero.jose79@gmail.com>
2026-01-15 09:00:51 -0300

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,6 @@
+# core/__init__.py
+from .procesamiento import ProcesadorArchivos
+from .calculos import CalculadoraResultados
+from .utils import formatear_monto
+
+__all__ = ['ProcesadorArchivos', 'CalculadoraResultados', 'formatear_monto']
