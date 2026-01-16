4dc6c5fb849341de9ba81cbbeb5f580764a9c8e0
Ki11Cr0w <romero.jose79@gmail.com>
2026-01-16 11:55:59 -0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -2,5 +2,11 @@
 from .procesamiento import ProcesadorArchivos
 from .calculos import CalculadoraResultados
 from .utils import formatear_monto
+from .visualizaciones import VisualizadorResultados  # NUEVO
 
-__all__ = ['ProcesadorArchivos', 'CalculadoraResultados', 'formatear_monto']
+__all__ = [
+    'ProcesadorArchivos', 
+    'CalculadoraResultados', 
+    'formatear_monto',
+    'VisualizadorResultados'  # NUEVO
+]
